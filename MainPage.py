0d22e3c0ebd6a9aa5940efa2dae549d8713f3c12
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-08-02 13:11:26 +0700

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -14,11 +14,11 @@ st.markdown("""
 - Raditya Rayhan Anjaya sebagai Programmer & Pengembang
 - Jhordy Alexander sebagai Penyusun
 ### Apa Itu KAKURU?
-**KAKURU** adalah sebuah mesin kalkulator yang berkerja layaknya kalkulator pada umumnya, hanya saja dirancang untuk mempermudah menyelesaikan masalah mengenai rumus matematika terutama dalam materi Bangun Ruang.
-### Gimana cara kerjanya?
+**KAKURU** adalah sebuah mesin kalkulator yang berkerja layaknya kalkulator pada umumnya, hanya saja dirancang untuk mempermudah siswa dalam menyelesaikan masalah mengenai rumus matematika terutama dalam materi Bangun Ruang, di sini siswa dapat mengecek hasil dari jawawban yang sudah mereka jawab.
+""")
+st.subheader("### Gimana cara kerjanya?")
+st.title("""
 Gampang! Cuman pilih lewat sidebar di sini👈 terus tinggal satset satset deh.
 """)
-
-st.title("Dibuat pada 2 Agustus 2025")
-
+st.subheader("Dibuat pada 2 Agustus 2025")
 st.caption("NOTE : APLIKASI INI MASIH DALAM TAHAP PENGEMBANGAN !!!")
