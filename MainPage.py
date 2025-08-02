448460b7765147ef2d386e931009d304c3c1e850
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-08-02 13:00:45 +0700

    Add files via upload

---
+++ MainPage.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="カクル/KAKURU MASTER",
+    page_icon=":tada:",
+    layout="wide"
+    )
+
+st.sidebar.success("👆SILAHKAN PILIH👆")
+
+st.title("KAKURU (Kalkulator Rumus): Cara mudah menghitung cepat soal matematika dan fisika")
+st.markdown("""
+    Orang yang terlibat :
+- Raditya Rayhan Anjaya sebagai Programmer
+- Khalil Ghibran Dzakwan Mulyadi sebagai Penyusun
+### Apa Itu KAKURU?
+**KAKURU** adalah sebuah mesin kalkulator yang berkerja layaknya kalkulator pada umumnya, hanya saja dirancang untuk mempermudah menyelesaikan masalah mengenai rumus matematika.
+### Gimana cara kerjanya?
+Gampang! Cuman pilih lewat sidebar di sini👈 terus tinggal satset satset deh.
+""")
+
+st.subheader("Jika ada bug atau masalah silahkan langsung report ke email dibawah ini :")
+st.markdown("""
+Bug report ke email dibawah ini 👇👇 
+- kakurudev@gmail.com
+""")
+
+st.header("Apa saja yang baru ?")
+st.markdown("""
+- Pembaruan KAKURU MASTER
+- Fix beberapa bug
+- Penambahan baru yaitu rumus-rumus Fisika (Yang bersangkutan dengan IPA)
+""")
+st.subheader("Diupdate pada tanggal 21 Februari 2025")
+
+st.caption("NOTE : APLIKASI INI MASIH DALAM TAHAP PENGEMBANGAN !!!")
