44e2e227d0dd90a6ba8b38e8ecff7b897a75c7cd
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-08-09 11:20:30 +0700

    Update BANGUN_DATAR_PERSEGI_PANJANG.py

--- pages/BANGUN_DATAR_PERSEGI_PANJANG.py
+++ pages/BANGUN_DATAR_PERSEGI_PANJANG.py
@@ -3,7 +3,14 @@ import streamlit as st
 st.title("PERSEGI PANJANG")
 st.markdown("""
 ### RUMUS :
-
+- K = Keliling
+- L = Luas
+- p = Panjang
+- l = Lebar
+### RUMUS MENCARI KELILING :
+- K = 2 x (p + l)
+### RUMUS MENCARI LUAS :
+- L = p x l
 """)
 
 p = st.number_input("Masukan Nilai Panjang:")
@@ -16,4 +23,5 @@ st.header("MENCARI PANJANG")
 st.header("LUAS")
 luas = p * l
 st.subheader(luas)
+
 
