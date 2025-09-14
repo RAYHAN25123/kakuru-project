3722e9e0e90473a4f87329de14a94c83b0f4780c
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-09-14 12:52:31 +0700

    Create APA_ITU _BANGUN_DATAR_?.py

---
+++ pages/APA_ITU _BANGUN_DATAR_?.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header("📐 Bangun Datar")
+st.subheader("Apa itu Bangun Datar?")
+st.markdown("""
+###📐 Bangun Datar
+- Bangun datar adalah bangun dua dimensi (hanya memiliki panjang dan lebar).
+""")
