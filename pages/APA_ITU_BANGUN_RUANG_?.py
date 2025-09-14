b17aca3f6b9db78bcb2d1878c46e47f29142bf8b
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-09-14 12:55:14 +0700

    Create APA_ITU_BANGUN_RUANG_?.py

---
+++ pages/APA_ITU_BANGUN_RUANG_?.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header("📦 Bangun Ruang")
+st.subheader("Apa itu bangun ruang?")
+st.markdown("""
+###📦 Bangun Ruang
+Bangun ruang adalah bangun tiga dimensi (punya panjang, lebar, tinggi, dan volume).
+""")
