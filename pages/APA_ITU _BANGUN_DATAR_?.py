2166ce9f9b35bfc27f41ad53d5a0b47ca6e5c7d7
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-09-14 12:56:02 +0700

    Update APA_ITU _BANGUN_DATAR_?.py

--- pages/APA_ITU _BANGUN_DATAR_?.py
+++ pages/APA_ITU _BANGUN_DATAR_?.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.header("📐 Bangun Datar")
 st.subheader("Apa itu Bangun Datar?")
 st.markdown("""
-###📐 Bangun Datar
+### 📐 Bangun Datar
 - Bangun datar adalah bangun dua dimensi (hanya memiliki panjang dan lebar).
 """)
