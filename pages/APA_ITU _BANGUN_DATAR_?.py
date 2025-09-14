88ea5d950ac09950890f79b48d2e2866dc395a01
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-09-14 13:04:47 +0700

    Update APA_ITU _BANGUN_DATAR_?.py

--- pages/APA_ITU _BANGUN_DATAR_?.py
+++ pages/APA_ITU _BANGUN_DATAR_?.py
@@ -7,7 +7,7 @@ st.markdown("""
 """)
 
 st.markdown("""
-Nah, sekarang udah tau kan apa itu bangun datar, setelah ini mari kita mengetahui lagi lebih dalam tentang bangun datar.
+### Nah, sekarang udah tau kan apa itu bangun datar, setelah ini mari kita mengetahui lagi lebih dalam tentang bangun datar.
 """)
 
 st.sidebar.success("👆SILAHKAN PILIH👆")
