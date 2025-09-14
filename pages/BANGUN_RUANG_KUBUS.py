81421826726160e797b76539f31986b0ee2d2086
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-09-14 13:10:54 +0700

    Update BANGUN_RUANG_KUBUS.py

--- pages/BANGUN_RUANG_KUBUS.py
+++ pages/BANGUN_RUANG_KUBUS.py
@@ -1,12 +1,29 @@
 import streamlit as st
 
 st.title("KUBUS")
+st.markdown("""
+### RUMUS :
+- p = Panjang
+- l = Lebar
+- t = Tinggi
+- s = Sisi
+- K = Keliling
+- V = Volume
+- LP = Luas Permukaan
+### RUMUS MENCARI KELILING :
+- K = 4 x sisi
+### RUMUS MENCARI VOLUME :
+- V = sisi³
+### RUMUS MENCARI LUAS PERMUKAAN :
+- LP = 6 × sisi²
+""")
+
 sisi = st.number_input("Masukan Panjang Sisi:")
 st.header("KELILING")
-k = 12 * sisi
+k = 4 * sisi
 st.subheader(k)
 st.header("LUAS")
-l = 6 * sisi
+l = sisi **3
 st.subheader(l)
 st.subheader("VOLUME")
 v = sisi * sisi * sisi
