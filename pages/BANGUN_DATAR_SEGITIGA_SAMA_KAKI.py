a6b4cb2d5eab7862d3938785ed28e508e32ab8a4
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-09-19 19:26:43 +0700

    Update BANGUN_DATAR_SEGITIGA_SAMA_KAKI.py

--- pages/BANGUN_DATAR_SEGITIGA_SAMA_KAKI.py
+++ pages/BANGUN_DATAR_SEGITIGA_SAMA_KAKI.py
@@ -16,11 +16,10 @@ st.markdown("""
 - t = √sisi2 - (a/2)2
 """)
 
-s1 = st.number_input("Masukan Panjang Sisi 1:")
-s2 = st.number_input("Masukan Panjang Sisi 2:")
-s3 = st.number_input("Masukan Panjang Sisi 3:")
+s = st.number_input("Masukan Sisi")
+a = st.number_input("Masukan Alas")
 st.header("KELILING")
-k = s1 + s2 + s3
+k = 2 * s + a
 st.subheader(k)
 
 a = st.number_input("Masukan Nilai Alas:")
@@ -28,3 +27,5 @@ t = st.number_input("Masukan Nilai Tinggi:")
 st.header("LUAS")
 l = 1 / 2 * a * t
 st.subheader(l)
+
+st.subheader("Rumus mencari Tinggi (Jika tidak ada)")
