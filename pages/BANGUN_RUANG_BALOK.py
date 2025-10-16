65dc90c420d46a7f46fc7aaa3a0569d9f047e87d
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-16 13:00:57 +0700

    Update BANGUN_RUANG_BALOK.py

--- pages/BANGUN_RUANG_BALOK.py
+++ pages/BANGUN_RUANG_BALOK.py
@@ -6,11 +6,8 @@ st.markdown("""
 - p = Panjang
 - l = Lebar
 - t = Tinggi
-- K = Keliling
 - L = Luas
 - V = Volume
-### RUMUS MENCARI KELILING :
-- K = 4 x ( p + l + t )
 ### RUMUS MENCARI LUAS :
 - L = 2 x (p x l) + (p x t) + (l x t)
 ### RUMUS MENCARI VOLUME :
@@ -20,9 +17,7 @@ st.markdown("""
 p = st.number_input("Masukan Nilai Panjang")
 l = st.number_input("Masukan Nilai Lebar")
 t = st.number_input("Masukan Nilai Tinggi")
-st.header("KELILING")
-k = 4 * ( p + l + t )
-st.subheader(k)
+
 st.header("LUAS BALOK")
 l1 = 2 * (p * l + p * t + l * t)
 st.subheader(l1)
