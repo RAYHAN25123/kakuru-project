ab93f5a354b24a7bcac32ce4c0fe771491917d94
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-16 12:58:42 +0700

    Update BANGUN_RUANG_BOLA.py

--- pages/BANGUN_RUANG_BOLA.py
+++ pages/BANGUN_RUANG_BOLA.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import math
 
-st.header("KELILING BOLA")
-r = st.number_input("Masukan Panjang Jari - jari")
-k = 2 * 22/7 * r
-kk = 2 * 3.14 * r
-st.write(k, " & ", kk)
-
 st.header("LUAS BOLA")
 l = 4 * 22/7 * r**2
 ll = 4 * 3.14 * r**2
