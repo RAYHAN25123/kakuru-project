64ad8781d4fc1f3b6392bbe99402afaf1d98a5db
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-16 13:29:19 +0700

    Update BANGUN_DATAR_SEGITIGA_SAMA_KAKI.py

--- pages/BANGUN_DATAR_SEGITIGA_SAMA_KAKI.py
+++ pages/BANGUN_DATAR_SEGITIGA_SAMA_KAKI.py
@@ -2,7 +2,7 @@ import streamlit as st
 import math
 
 st.title("SEGITIGA SAMA KAKI")
-st.image_url = "https://images.unsplash.com/photo-1548247738-4e3a9d201a07"
+st.image_url = "https://www.google.com/search?sca_esv=415e83c541284ed9&rlz=1C1GCEA_enID1178ID1178&sxsrf=AE3TifMrIp0aPW671ALM_WnVi6Oi0wb0Qw:1760595863192&udm=2&fbs=AIIjpHxgbM2h8Svo6qNug7XqS5BsfddAIs86QJoqf6s-CtEaq0j8ejD2V3ygKxRPty1SN0H5YSUP8xy1LPYJwQDIm5mIKRjMoy8d_PSOR0zjq1xXnX-YLxYKioJGCvsrZ9GnyAoXagQ1PyyQus8n-PlkCX_Xr_zxHgrL5H6labWG7Q6BUF2PDrYfQu4fo1U-ZbEQBwcdlg0uvHSavZ_k0jeXcVJBungzqQ&q=segitiga+sama+kaki&sa=X&sqi=2&ved=2ahUKEwjk5ZLYiqiQAxWtyzgGHatvOXoQtKgLegQIDhAB"
 st.markdown("""
 ### RUMUS :
 - s = Sisi
