3738e90541d8dec63df245141da70b2f61c91cd7
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-19 19:51:57 +0700

    Add files via upload

---
+++ pages/BANGUN_RUANG_TABUNG.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("TABUNG")
+st.markdown("""
+### Penjabaran :
+- V = Volume
+- r = Jari-jari
+- t = tinggi
+- π = 3,14 atau 22/7
+### Rumus Mencari Volume :
+- V = π x r^2 x t
+""")
+st.caption("Rumus ini menggunakan π = 3,14")
+r = st.number_input("Masukan Jari-jari")
+t = st.number_input("Masukan Tinggi")
+v = 3,14 * r **2 * t
+st.header("Volume")
+st.subheader(v)("cm")
