8525b582adf49f4fe42ff273149432cd8360d62b
R4yh@n_ <155223792+RAYHAN25123@users.noreply.github.com>
2025-10-30 06:21:46 +0700

    Update BANGUN_DATAR_SEGITIGA_SIKU_SIKU.py

--- pages/BANGUN_DATAR_SEGITIGA_SIKU_SIKU.py
+++ pages/BANGUN_DATAR_SEGITIGA_SIKU_SIKU.py
@@ -19,3 +19,25 @@ st.markdown("""
 ### RUMUS MENCARI TINGGI (Jika tidak ada tinggi) :
 - t = √sisi2 - (a/2)2
 """)
+
+s = st.number_input("Masukan Sisi", key="sisi 1")
+a = st.number_input("Masukan Alas", key="alas 1")
+st.header("KELILING")
+k = 2 * s + a
+st.subheader(k)
+
+a1 = st.number_input("Masukan Nilai Alas", key="alas 2")
+t = st.number_input("Masukan Nilai Tinggi", key="tinggi 1")
+st.header("LUAS")
+l = 1 / 2 * a1 * t
+st.subheader(l)
+
+st.subheader("Rumus mencari Tinggi (Jika tidak ada)")
+s1 = st.number_input("Masukan Sisi", key="sisi 2")
+a2 = st.number_input("Masukan Alas", key="alas 3")
+t1 = s1 **2
+t2 = (a2 /2) **2
+t3 = t1 - t2
+t4 = math.sqrt(t3)
+st.header("Tinggi")
+st.subheader(t4)
